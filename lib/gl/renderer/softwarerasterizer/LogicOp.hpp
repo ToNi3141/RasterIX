3c8bfd75f6e386766346e0c439073b8307387c44
ToNi3141 <>
2026-01-19 21:41:40 +0100

    improve performance

--- lib/gl/renderer/softwarerasterizer/LogicOp.hpp
+++ lib/gl/renderer/softwarerasterizer/LogicOp.hpp
@@ -28,7 +28,20 @@ namespace rr::softwarerasterizer
 class LogicOp
 {
 public:
-    Vec4 op(const Vec4& src, const Vec4& dst) const;
+    Vec4 op(const Vec4& src, const Vec4& dst) const
+    {
+        if (!m_enable)
+            return src;
+
+        const Vec4ui8 srcInt = Vec4ui8::createFromVec<Vec4, 8>(src);
+        const Vec4ui8 dstInt = Vec4ui8::createFromVec<Vec4, 8>(dst);
+        const Vec4ui8 resultInt = calcOp(srcInt, dstInt);
+        const Vec4 result { static_cast<float>(resultInt[0]) / 255.0f,
+            static_cast<float>(resultInt[1]) / 255.0f,
+            static_cast<float>(resultInt[2]) / 255.0f,
+            static_cast<float>(resultInt[3]) / 255.0f };
+        return result;
+    }
 
     void setLogicOp(const rr::LogicOp logicOp)
     {
@@ -51,7 +64,65 @@ private:
         return Vec4ui8 { 255, 255, 255, 255 } - color;
     }
 
-    Vec4ui8 calcOp(const Vec4ui8& src, const Vec4ui8& dst) const;
+    Vec4ui8 calcOp(const Vec4ui8& src, const Vec4ui8& dst) const
+    {
+        Vec4ui8 result;
+        switch (m_logicOp)
+        {
+        case rr::LogicOp::CLEAR:
+            result = Vec4ui8 { 0, 0, 0, 0 };
+            break;
+        case rr::LogicOp::SET:
+            result = Vec4ui8 { 255, 255, 255, 255 };
+            break;
+        case rr::LogicOp::COPY:
+            result = src;
+            break;
+        case rr::LogicOp::COPY_INVERTED:
+            result = invertColor(src);
+            break;
+        case rr::LogicOp::NOOP:
+            result = dst;
+            break;
+        case rr::LogicOp::INVERT:
+            result = invertColor(dst);
+            break;
+        case rr::LogicOp::AND:
+            result = src & dst;
+            break;
+        case rr::LogicOp::NAND:
+            result = invertColor(src & dst);
+            break;
+        case rr::LogicOp::OR:
+            result = src | dst;
+            break;
+        case rr::LogicOp::NOR:
+            result = invertColor(src | dst);
+            break;
+        case rr::LogicOp::XOR:
+            result = src ^ dst;
+            break;
+        case rr::LogicOp::EQUIV:
+            result = invertColor(src ^ dst);
+            break;
+        case rr::LogicOp::AND_REVERSE:
+            result = src & invertColor(dst);
+            break;
+        case rr::LogicOp::AND_INVERTED:
+            result = invertColor(src) & dst;
+            break;
+        case rr::LogicOp::OR_REVERSE:
+            result = src | invertColor(dst);
+            break;
+        case rr::LogicOp::OR_INVERTED:
+            result = invertColor(src) | dst;
+            break;
+        default:
+            result = src; // Fallback
+            break;
+        }
+        return result;
+    }
 
     bool m_enable { false };
     rr::LogicOp m_logicOp { rr::LogicOp::COPY };
