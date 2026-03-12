726e827f11ff5f2b7a851fb940d87492cc1f0e02
ToNi3141 <>
2026-03-12 23:50:22 +0100

    Improve image quality by optimizing the interpolation

--- lib/gl/renderer/softwarerasterizer/AttributeInterpolator.hpp
+++ lib/gl/renderer/softwarerasterizer/AttributeInterpolator.hpp
@@ -51,6 +51,11 @@ private:
         return attrStart + (attrIncX * bbx) + (attrIncY * bby);
     }
 
+    static int32_t mulh(const int32_t a, const int32_t b)
+    {
+        return static_cast<int32_t>((static_cast<int64_t>(a) * static_cast<int64_t>(b)) >> 32);
+    }
+
     static InterpolatedAttributesData::Texture interpolateTexture(
         const TriangleStreamTypes::TextureX& texture,
         const int32_t bbx,
@@ -59,31 +64,31 @@ private:
         // This interpolation has a high impact on the image quality.
         // More precision here would result in a less distorted image.
         // The tradeoff here is between performance and visual fidelity.
-        // We choose performance over visual fidelity.
-        // For instance, 64 bit calculations would increase the fidelity to a noticeable degree.
+        // Hopefully there is a good tradeoff between performance and fidelity.
+        // We chose the calculation here wisely. We multiply two 32 bit values which results in a 64 bit value.
+        // The shift is exactly 32 bit. So the compiler should use a special instruction for that like a MULH instruction
+        // in the RiscV instruction set or the SMUL instruction in the ARM instruction set.
 
         int32_t s = interpolateAttribute(texture.texStq[0], texture.texStqXInc[0], texture.texStqYInc[0], bbx, bby); // S3.28
         int32_t t = interpolateAttribute(texture.texStq[1], texture.texStqXInc[1], texture.texStqYInc[1], bbx, bby); // S3.28
         int32_t q = interpolateAttribute(texture.texStq[2], texture.texStqXInc[2], texture.texStqYInc[2], bbx, bby); // S3.28
 
-        s = s >> 15; // S3.28 -> S3.13 Note: Hardware uses S3.20
-        t = t >> 15; // S3.28 -> S3.13 Note: Hardware uses S3.20
-        q = q >> 15; // S3.28 -> S3.13 Note: Hardware uses S3.21
+        // s S3.28 Note: Hardware uses S3.20
+        // t S3.28 Note: Hardware uses S3.20
+        q = q >> 17; // S3.28 -> S3.11 Note: Hardware uses S3.21
 
         if (q != 0)
         {
-            q = (static_cast<int32_t>(1) << 24) / q; // S1.24 / S3.13 -> Sx.11
+            q = (static_cast<int32_t>(1) << 30) / q; // S1.30 / S3.11 -> Sx.19
         }
         else
         {
             q = std::numeric_limits<int32_t>::max();
         }
 
-        s *= q; // Sx.13 * Sx.11 -> Sx.24
-        t *= q; // Sx.13 * Sx.11 -> Sx.24
-        s = s >> 9; // Sx.24 -> Sx.15
-        t = t >> 9; // Sx.24 -> Sx.15
-        q <<= 4;
+        s = mulh(s, q); // Sx.28 * Sx.19 -> Sx.47; mulh will use the upper 32 bit -> Sx.47 >> 32 -> Sx.15
+        t = mulh(t, q); // Sx.28 * Sx.19 -> Sx.47; mulh will use the upper 32 bit -> Sx.47 >> 32 -> Sx.15
+        q <<= 6;
 
         return InterpolatedAttributesData::Texture { s, t, q };
     }
